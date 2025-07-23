396964791040b0fedbcb5bba3762da3f611bfa49
fangs25 <fangsheng_ruc@163.com>
2025-07-23 14:48:02 +0800

    Update main.py

--- scHSC/main.py
+++ scHSC/main.py
@@ -2,7 +2,7 @@ from .network import HSCNetwork, ZINBLoss, EarlyStopping
 from .graph_function import get_adj,get_appro_adj
 from .utils import *
 from .logger import create_logger     
-
+from tqdm import tqdm
 
 import torch
 from torch import optim
@@ -11,8 +11,11 @@ import os
 import numpy as np
 from torch.optim.lr_scheduler import ExponentialLR
 import warnings
-
+from torch.utils.data import DataLoader, BatchSampler
 warnings.filterwarnings("ignore")
+import numpy as np
+import scipy
+from scipy.sparse import csr_matrix
 
 
 class scHSCModel:
@@ -29,75 +32,106 @@ class scHSCModel:
             self.log.info('Create log file...')
             self.log.info('Create scHSCModel Object Done...')
 
-    def preprocess(self, adata, t = 1, highly_genes = 2000, k = 18, preprocessed = False, lap__filter = True, approx = True, lap_sparse = True):
-        """
-        The `preprocess` function takes in an `adata` object and performs various preprocessing steps
-        including normalization, filtering, and calculating adjacency matrix.
-
-        :param adata: Anndata object that contains the gene expression data. 
-        :param t: Used in the function "laplacian_filtering". 
-            It represents the number of iterations for the filtering process. 
-            The higher the value of "t", the more smoothing will be applied to the data. 
-        :param highly_genes: Specify the number of highly variable genes to 
-            consider during the normalization step. 
-        :param k: Number of nearest neighbors to consider 
-            when constructing the adjacency matrix.
-        :param preprocessed: Indicates whether the input data has already been preprocessed.
-        :param lap__filter: Indicates whether to apply Laplacian filtering to the data.
-        :param approx: Indicates whether to use an approximate method for constructing the adjacency matrix.
-        :param lap_sparse: Indicates whether to use a sparse matrix representation for the Laplacian filtering.
-        
-        :return: adata, processed anndata.
-        """
-
+    def preprocess(self, adata, t=1, highly_genes = 2000, k = 18, batch_str = None, lap__filter = True, approx = True, lap_sparse = True,
+                   filter_min_counts=True, size_factors=True, normalize_input=True, logtrans_input=True):
         setup_seed(5)
-        if not preprocessed: 
-            if self.info:
-                self.log.info('Preprocessing data...')
 
-            adata = normalize(adata, filter_min_counts=True, highly_genes=highly_genes,
-                                size_factors=True, normalize_input=True, 
-                                logtrans_input=True, info = self.info, log = self.log) 
-            self.raw_count = torch.tensor(adata.raw[:,np.array(adata.var.highly_variable.index, dtype = np.int16)].X).to(self.device)
-            if approx:
-                if self.info:
-                    self.log.info("Constructing approximate adjacency matrix using KNN...")
-                A = get_appro_adj(adata.X, k=k)
-            else:
-                if self.info:
-                    self.log.info("Constructing exact adjacency matrix using KNN...")
-                A, _ = get_adj(adata.X, k = k)
-            self.A = torch.tensor(A).float()
+        if adata.X.shape[0] > 15000:
+            if self.info:
+                self.log.info('Sparsing raw data...')
+            adata.X = scipy.sparse.csr_matrix(adata.X)
+
+        if self.info:
+            self.log.info('Preprocessing data...')
+
+        if size_factors or normalize_input or logtrans_input:
+            adata.raw = adata.copy()
+        else:
+            adata.raw = adata
+            
+        if filter_min_counts:
+            if self.info:
+                self.log.info(f'Filtering genes({adata.X.shape[1]}) and cells({adata.X.shape[0]})...')
+            sc.pp.filter_genes(adata, min_counts=1)
+            sc.pp.filter_cells(adata, min_counts=1)
+
+        if size_factors:
+            if self.info:
+                self.log.info('Normalizing data...')
+            sc.pp.normalize_total(adata)
+            adata.obs['size_factors'] = adata.obs.n_counts / np.median(adata.obs.n_counts)
+        else:
+            adata.obs['size_factors'] = 1.0
+
+        if logtrans_input:
+            if self.info:
+                self.log.info("Log1p data...")
+            sc.pp.log1p(adata)
+
+        if highly_genes != None:
+            if self.info:
+                self.log.info("Selecting HVG(n_top_genes={})...".format(highly_genes))
+            sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5, n_top_genes = highly_genes, subset=True)
+            # adata.uns['raw_counts'] = adata.raw[:, adata.var_names[adata.var['highly_variable']]].X
+
+
+        if normalize_input:
+            if self.info:
+                self.log.info("Scaling data...")
+            sc.pp.scale(adata)
+        
+        if approx:
+            if self.info:
+                self.log.info("Constructing approximate adjacency matrix using KNN...")
+            A = get_appro_adj(adata.X, k = k)
         else:
             if self.info:
-                self.log.info('Reading preprocessed data...')
-            self.raw_count = torch.tensor(adata.raw[:,np.array(adata.var.highly_variable.index, dtype = np.int16)].X).to(self.device)
-            self.A =  torch.tensor(adata.uns['A']).float()
+                self.log.info("Constructing exact adjacency matrix using KNN...")
+            A, _ = get_adj(adata.X, k = k)
 
-        setup_seed(5)  
+        if batch_str != None:
+            if self.info:
+                self.log.info(f'Constructing adjacency matrix using {A.sum()} KNN pairs...')
+            sc.pp.pca(adata) 
+            emb_matrix = adata.obsm['X_pca']
+            batch_index = adata.obs[batch_str].values
+            from .calculate_NN import get_dict_mnn_para
+            mnn_inter_batch_approx = get_dict_mnn_para(data_matrix=emb_matrix,batch_index=batch_index,k=10,flag="out",
+                                    metric='cosine',approx=True,return_distance=False,njob=1)
+            mnn_inter_batch=np.array(mnn_inter_batch_approx)
+            NN_matrix = np.zeros((adata.X.shape[0], adata.X.shape[0]))
+            indices = tuple(zip(*mnn_inter_batch))
+            np.add.at(NN_matrix, indices, 1)
+            A = NN_matrix + A
+            if self.info:
+                self.log.info(f'Correcting adjacency matrix using {NN_matrix.sum()} MNN pairs...')
+
+        adata.uns['adjacency_matrix'] = A
 
         if lap__filter:
             if lap_sparse:
                 if self.info:
                     self.log.info('Laplacian filtering(sparse)...')
-                self.X_filtered = laplacian_filtering_sparse(self.A, torch.tensor(adata.X).float(), t)
+                # X_filtered = laplacian_filtering_sparse(A, torch.tensor(adata.X).float(), t)
+                X_filtered = laplacian_filtering_sparse(A, adata.X, t)
+
             else:
                 if self.info:
                     self.log.info('Laplacian filtering...')
-                self.X_filtered = laplacian_filtering(self.A, torch.tensor(adata.X).float(), t)
-        else:
-            self.X_filtered = adata.X
+                X_filtered = laplacian_filtering(A, torch.tensor(adata.X).float(), t)
 
-        self.node_num = self.X_filtered.shape[0]
-        self.size_factor = adata.obs['size_factors']
+            adata.uns['laplacian_filtered'] = X_filtered
+        else:
+            adata.uns['laplacian_filtered'] = adata.X
 
         return adata
 
 
-    def train(self, adata, target_clusters = 8, dims = 32, batch_size = 500, drop_rate = 0.5,
-              iterations = 80, lr = 0.00001, sep = 3,  
+
+    def train(self, adata, target_clusters = 8, dims = 32, batch_size = 512, drop_rate = 0.5,
+              iterations = 100, lr = 0.00001, sep = 3,  #  lr=0.00001!
               alpha = 0.5, beta = 1, tau = 0.9, louvain = True, leiden = False, 
-              patience=5, delta=0):
+              patience=1, delta=0, warm_up = True):
         """
         Train the hard sample contrastive learning model.
 
@@ -121,65 +155,110 @@ class scHSCModel:
         Returns:
         - None. The function modifies the `adata` object in-place by adding new obsm fields and obs fields.
         """
+        self.batch_size = int(np.min((adata.X.shape[0] - 1, batch_size)))
+        self.dims = dims
+        if self.info:
+            self.log.info(f"Initializing Dataset...")
+        dataset = GraphDataset(adata)
+        sampler = InfiniteRandomSampler(adata.n_obs, self.batch_size)
+        batch_sampler = BatchSampler(sampler, batch_size=self.batch_size, drop_last=True)
+        if self.info:
+            self.log.info(f"Initializing Train DataLoader...")
+        dataloader = DataLoader(
+            dataset,
+            batch_sampler=batch_sampler,
+            num_workers=2,
+            pin_memory=True
+        )
+        if self.info:
+            self.log.info(f"Initializing Evaluation DataLoader...")
+        eval_dataloader = DataLoader(dataset, batch_size = 5102, shuffle=False, num_workers=2, pin_memory=True)
 
-        self.batch_size = int(np.min((self.X_filtered.shape[0]-1, batch_size)))
-        self.model = HSCNetwork(input_dim = self.X_filtered.shape[1], dataset_size = self.X_filtered.shape[0], 
-                                hidden_dim = dims, batch_size = self.batch_size, drop_rate = drop_rate, device = self.device)
+        if self.info:
+            self.log.info(f"Initializing HSCNetwork...")
+        self.model = HSCNetwork(input_dim = adata.X.shape[1], dataset_size = adata.X.shape[0], 
+                                hidden_dim = self.dims, batch_size = self.batch_size, drop_rate = drop_rate, device = self.device)
 
         if self.info:
             self.log.info(f"Classify the data into {target_clusters} distinct clusters...")
-        self.A, self.X_filtered = map(lambda x: torch.tensor(x).to(self.device), (self.A, self.X_filtered))
         self.model.to(self.device)
 
         optimizer = optim.Adam(self.model.parameters(), lr=lr)
         scheduler_ExponentialLR = ExponentialLR(optimizer, gamma=0.98)
+
 
         mask_11 = (torch.ones([self.batch_size, self.batch_size]) - torch.eye(self.batch_size)).to(self.device)
         mask_12 = torch.ones([self.batch_size, self.batch_size]).to(self.device)
         
         early_stopping = EarlyStopping(patience=patience, delta=delta)
         self.loss_list = []
+        iterator = iter(dataloader)
 
         for iteration in range(iterations):
             self.model.train()
-            idx = np.random.choice(range(self.X_filtered.shape[0]), self.batch_size, replace=False)
-            X_filtered_sample = self.X_filtered[idx,:]
-            A_sample = self.A[idx,:]
 
-            Z1, Z2, E1, E2, _mean, _disp, _pi = self.model(X_filtered_sample, A_sample)
+            batch = next(iterator)
+
+            x_batch = batch['laplacian_filtered'].to(self.device)
+            a_batch = batch['adjacency_matrix'].to(self.device)
+            raw_batch = batch['raw_count'].to(self.device)
+            sf_batch = batch['size_factor'].to(self.device)
+
+            Z1, Z2, E1, E2, _mean, _disp, _pi = self.model(x_batch, a_batch)
+
+
+            if warm_up:
+                Z = (Z1 + Z2) / 2
+                P, center = phi(Z, target_clusters, leiden = leiden, louvain = louvain)
+                compactness_scale, separation_scale = compute_compactness_separation(Z.detach().cpu().numpy(), P.astype(int).astype(str), scale = True, normalize = True)
+                self.log.info(f'iteration:{iteration:<3d}, alpha:{alpha}, lr:{lr}, compactness_norm:{compactness_scale:.6f}, separation_norm:{separation_scale:.6f}')
+                
+                if compactness_scale > 20 and separation_scale < 100:
+                    alpha = 0.95
+                    weight = 10
+                elif compactness_scale < 20 and separation_scale < 100:
+                    weight = 100
+                else:
+                    alpha = 0.5
+                    weight = 0.1
+
+            # self.log.info(f'iteration:{iteration:<3d}, alpha:{alpha}, weight:{weight}')
 
             ZE_11,ZE_12,ZE_22 = self.comprehensive_similarity(Z1, Z2, E1, E2, alpha)
+
+
 
             contrastive_loss = self.hard_sample_infoNCE(ZE_11, ZE_12, ZE_22, 
                                                    self.model.pos_neg_weight_11, self.model.pos_neg_weight_12, 
                                                    self.model.pos_neg_weight_22, self.model.pos_weight, self.batch_size,
                                                    mask_11, mask_12)
             
-            ZINB_Loss = ZINBLoss(pi = _pi, disp = _disp, scale_factor = torch.tensor(self.size_factor)[idx].to(self.device))
-            zinb_loss = ZINB_Loss(x = self.raw_count[idx,:], mean = _mean)
+            ZINB_Loss = ZINBLoss(pi = _pi, disp = _disp, scale_factor = sf_batch)
+            zinb_loss = ZINB_Loss(x = raw_batch, mean = _mean)
+
 
             Wzinb = (contrastive_loss / zinb_loss).detach().to(self.device) # detach the Wzinb for preventing breaking graph
-
-            loss = contrastive_loss + Wzinb*zinb_loss
-            loss.backward()
+            loss = (contrastive_loss + Wzinb*zinb_loss)
+            total_loss = weight * loss
+            total_loss.backward()
             optimizer.step()
-            scheduler_ExponentialLR.step()
+
+
             self.loss_list += [loss.item()]
             
             if iteration % sep == 0:
-                if self.info:
-                    self.log.info(f'iteration:{iteration:<3d}, loss:{loss:.6f}')
+                scheduler_ExponentialLR.step()
 
                 # evaluation mode
                 self.model.eval()
-                Z1, Z2, E1, E2, _, _, _ = self.model(X_filtered_sample, A_sample)
+                Z1, Z2, E1, E2, _, _, _ = self.model(x_batch, a_batch)
 
                 ZE_11, ZE_12, ZE_22 = self.comprehensive_similarity(Z1, Z2, E1, E2, alpha)
 
                 # fusion and testing
                 Z = (Z1 + Z2) / 2
 
-                P, center = phi(Z, target_clusters, leiden = leiden, louvain = louvain)
+                P, center = phi(Z, target_clusters, leiden = leiden, louvain = louvain)                
                 P, center = map(lambda x: torch.tensor(x).to(self.device), (P, center))
 
                 # select high confidence samples
@@ -194,30 +273,60 @@ class scHSCModel:
                 self.model.pos_neg_weight_22[H_mat] = M_mat_22[H_mat].data
 
                 self.model.eval()
-                Z1_full, Z2_full = self.model.forward_full(self.X_filtered) # time
-                Z_full = (Z1_full + Z2_full) / 2
+                if self.info:
+                    self.log.info(f'iteration:{iteration:<3d}, loss:{loss:.6f}')
+                
+                # if self.info:
+                    # self.log.info(f'iteration:{iteration:<3d}, loss:{loss:.6f}, alpha:{alpha}, lr:{lr}, nmi:{nmi:.6f}, ari:{ari:.6f}, acc:{acc:.6f}, ss:{ss:.6f}, chs:{chs:.6f}, compactness:{compactness:.6f}, compactness_scale:{compactness_scale:.6f}, compactness_norm:{compactness_norm:.6f}, separation:{separation:.6f}, separation_scale:{separation_scale:.6f}, separation_norm:{separation_norm:.6f}')
+                    # self.log.info(f'iteration:{iteration:<3d}, loss:{loss:.6f}, nmi:{nmi:.6f}, ari:{ari:.6f}, acc:{acc:.6f}, compactness_norm:{compactness_norm:.6f}, separation_norm:{separation_norm:.6f}')
 
                 early_stopping(loss)
-                if early_stopping.early_stop:
+
+                if early_stopping.early_stop :
                     self.log.info(f"----------Early stopping in {iteration:<3d}th/{iterations:<3d}.--------")
+                    self.log.info("Training complete!")
+
                     self.model.eval()
-                    Z1_full, Z2_full = self.model.forward_full(self.X_filtered)
-                    Z_full = (Z1_full + Z2_full) / 2
-                    clusters, _ = phi(Z_full, target_clusters, leiden = leiden, louvain = louvain, )
+                    Z_full = self.predict_full(eval_dataloader)
+                    
+                    clusters, _ = phi(Z_full, target_clusters, leiden = leiden, louvain = louvain, log = self.log)
+                    self.log.info(f"----------Clustering Done!--------")
+
                     break
                 
 
             if iteration == iterations - 1: # Final Evaluation
                 self.model.eval()
-                Z1_full, Z2_full = self.model.forward_full(self.X_filtered)
-                Z_full = (Z1_full + Z2_full) / 2
-                clusters, _ = phi(Z_full, target_clusters, leiden = leiden, louvain = louvain, )
+                Z_full = self.predict_full(eval_dataloader)
 
-        self.log.info("Training complete")
+                clusters, _ = phi(Z_full, target_clusters, leiden = leiden, louvain = louvain,log=self.log)
+
 
         adata.obsm["X_schsc"] = Z_full.detach().cpu().numpy()
         adata.obs["reassign_cluster"] = clusters.astype(int).astype(str)
         adata.obs["reassign_cluster"] = adata.obs["reassign_cluster"].astype("category")
+    
+    
+    def predict_full(self, eval_dataloader):
+        self.model.eval()
+
+        total_size = len(eval_dataloader.dataset)
+        device = self.device
+        Z_full = torch.empty((total_size, self.dims), dtype=torch.float32, device=device)
+
+        with torch.no_grad():
+            start = 0
+            for batch in tqdm(eval_dataloader, desc="Predicting", leave=True):
+                x_batch = batch['laplacian_filtered'].to(device, non_blocking=True)
+                Z1, Z2 = self.model.forward_full(x_batch)
+                Z = (Z1 + Z2) / 2
+
+                end = start + Z.size(0)
+                Z_full[start:end] = Z  
+                start = end
+
+        return Z_full.cpu()
+
     
     
     def comprehensive_similarity(self, Z1, Z2, E1, E2, alpha):
@@ -350,8 +459,3 @@ class scHSCModel:
         M_1 = torch.diag(M_mat_12)
         return M_1, M_mat_11, M_mat_12, M_mat_22
     
-
-
-
-
-
